ffb91247f9001a196649d365633e75262cf8ce6b
Hussein Aqel <behmalik40@gmail.com>
2025-05-25 13:19:17 +0300

    Upgraded WebView Screen

--- lib/features/home/presentation/views/web_view_screen.dart
+++ lib/features/home/presentation/views/web_view_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:safty_children/core/themeing/app_colors.dart';
+import 'package:safty_children/core/themeing/app_styles.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class WebViewScreen extends StatefulWidget {
@@ -15,15 +18,22 @@ class _WebViewScreenState extends State<WebViewScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-      ..loadRequest(Uri.parse(widget.url));
+    _controller =
+        WebViewController()
+          ..setJavaScriptMode(JavaScriptMode.unrestricted)
+          ..loadRequest(Uri.parse(widget.url));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-   
+      appBar: AppBar(
+        backgroundColor: AppColors.appBarColor,
+        title: Text(
+          'Copilot AI',
+          style: AppStyles.appBarStyle.copyWith(fontSize: 24.sp),
+        ),
+      ),
       body: WebViewWidget(controller: _controller),
     );
   }
